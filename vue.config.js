b7541294c64b1bf072e29e3df101cd3eb68dd844
sangshaofeng <sangshaofeng@kungeek.com>
2019-07-26 18:10:55 +0800

    删除配置

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  devServer: {
-    host: "0.0.0.0",
-    port: 9000,
-    proxy: {
-      "/": {
-        target: "",
-        changeOrigin: true,
-        pathRewrite: {
-          "/": ""
-        }
-      }
-    }
-  },
+  devServer: {},
   productionSourceMap: false,
 }
